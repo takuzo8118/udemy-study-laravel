efedc496d023a63b41d0c884cfa3d7316bfa285c
takuzo8118 <saitou2930b@gmail.com>
2022-11-04 13:34:21 +0900

    コントローラーの実装

--- routes/web.php
+++ routes/web.php
@@ -12,25 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// デフォルトで呼ばれるルート
-Route::get('/', function () {
-    return view('welcome');
-});
-// クイズ一覧ページを返す
-Route::get('/quizzes', function () {
-    return view('quizzes.index');
-})->name('quizzes.index');
-
-//クイズ内容の詳細ページを返す
-Route::get('quizzes/show', function () {
-    return view('quizzes.show');
-})->name('quizzes.show');
-
-// 問題追加画面を返す
-Route::get('quizzes/create', function () {
-    return view('quizzes.create');
-})->name('quizzes.create');
-
-Route::delete('quizzes/{id}', function ($id) {
-    return json_encode(['message' => 'ID:'.$id.' が削除されるIDもです(仮の出力)']);
-})->name('quizzes.delete');
+Route::post('quizzes', 'App\Http\Controllers\QuizController@store')->name('quizzes.store');
